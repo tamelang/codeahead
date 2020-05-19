c2ed138d0cb9b118d14b51521b0b85eceac199d5
Tim Amelang <tim@tims-mbp.speedport.ip>
2020-05-19 17:37:16 +0200

    do it

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,99 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <header>
+      <div class="navbar">
+        <div class="logo">
+          <router-link to="/">Code Ahead</router-link>
+        </div>
+        <div class="burger">Burger Icon</div>
+      </div>
+      <ul class="nav flyout">
+        <li>
+          <router-link to="/about">Services</router-link>
+        </li>
+        <li>
+          <router-link to="/about">Projects</router-link>
+        </li>
+        <li>
+          <router-link to="/about">Technilogies</router-link>
+        </li>
+        <li>
+          <router-link to="/about">People</router-link>
+        </li>
+        <li>
+          <router-link to="/about">Career</router-link>
+        </li>
+      </ul>
+      <ul class="nav permanent-actions">
+        <li>
+          <router-link to="/projects">Explore</router-link>
+        </li>
+        <li>
+          <router-link to="/contact">Request</router-link>
+        </li>
+      </ul>
+    </header>
+    <article>
+      <router-view />
+    </article>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@use "./assets/styles/abstracts/abstracts";
+* {
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+.navbar {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  //background-color: $secondary;
+}
 
+.logo {
+  font-size: 54px;
+  align-self: flex-start;
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    //color: $primary;
+  }
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+.burger {
+  align-self: flex-end;
+  color: white;
+}
+
+.permanent-actions {
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  overflow: hidden;
+  width: 100%;
+  li {
+    display: flex;
+    color: white;
+    align-content: center;
+    padding: 1rem;
+    width: 50%;
+    a {
+      width: 100%;
+      text-align: center;
+      //color: $secondary;
     }
   }
+  li:nth-child(even) {
+    background-color: $fire-bush;
+  }
+  li:nth-child(odd) {
+    //background-color: $highlight2;
+  }
+}
+.flyout {
+  display: block;
+  //background-color: $background;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img src="https://placehold.it/1200x400" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {},
 };
 </script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+</style>
