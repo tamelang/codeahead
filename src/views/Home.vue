3310acbcb989dc7bc943176d77277a1eb0e3a8d0
Tim Amelang <tim@Tims-MacBook-Pro.local>
2020-05-20 14:52:08 +0200

    fix the build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,15 +16,49 @@
       </p>
       <h2>In my day job</h2>
       <p>
-        I am working with CMS systems e.g. Adobe Experience Manger (AEM).
-        Planning solution to maximise the usage of the out of the box
-        functionalities as well as bringing he best user experience for the
-        authors and endusers of the website.
+        I am working with CMS systems e.g. Adobe Experience Manger (AEM). Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Autem ad repellendus
+        reprehenderit labore neque dolore magni alias similique sequi molestiae
+        nam numquam, eius, non maiores fugit obcaecati expedita. Eligendi,
+        rerum.
       </p>
       <h2>In my free time</h2>
       <p>
-        I am working with my friends on a side project called
-        <a href="https://bruno.events">"Bruno - Events"</a>.
+        I am working with my friends in a team called
+        <a href="https://the-bash.de" target="_blank">BASH</a>. We modern web
+        applications and learn new technologies. One of out side projects is
+        called "Bruno Events". Click
+        <a href="https://bruno.events" target="_blank">here</a> to check it out.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolore
+        natus accusantium neque fugit iste, enim aperiam nesciunt in fugiat
+        consequatur quos praesentium eveniet voluptatum expedita, dolorem
+        suscipit cumque atque.
+      </p>
+    </article>
+    <article class="container">
+      <h1>Services</h1>
+      <p>
+        Let me share what I am offering.
+      </p>
+      <h2>Adobe Marketing Cloud</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, amet
+        veniam. Incidunt ratione, consequatur quos ea assumenda quam sint
+        corporis exercitationem nihil in. Quo possimus harum sapiente quae eaque
+        nulla.
+      </p>
+      <h2>Single Page Applications (SPA)</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga officia
+        beatae maxime architecto quisquam hic dolore maiores modi dolores quia
+        eaque, accusamus iure at eligendi impedit adipisci ipsum harum rem.
+      </p>
+      <h2>Multi Platform Mobile Apps</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eum
+        quae ullam, nostrum neque sed laudantium veritatis eveniet quo
+        perferendis nobis tempore quaerat harum possimus at sint rem totam
+        similique.
       </p>
     </article>
   </div>
@@ -35,7 +69,7 @@
 
 export default {
   name: "Home",
-  components: {}
+  components: {},
 };
 </script>
 
